1d91d3b3c08ec1bcb49c16cb339286320ca0744e
Joel Hobgood <jhobgoo@gmail.com>
2020-03-08 21:31:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,9 +35,10 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    if @@all.find_by_name.each do |song|
-      song.name == name
+    if @@all.find_by_name {|song| song.name == name}
       self.find_by_name
+    else
+      self.create_by_name
     end
   end
   
