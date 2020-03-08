ad2b118bdca1e12fa8b5c10d83a440f8e9d1a61b
Joel Hobgood <jhobgoo@gmail.com>
2020-03-08 20:51:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,29 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+  
+  def self.find_by_name(name)
+    @@all.find.each do |name|
+      song.name == name
+    end
+  end
+  
 end
